c0c491ef20c36ba839f134b1411960637f587ee1
baturfamily <goker.batur@gmail.com>
2026-05-25 12:02:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2010,6 +2010,7 @@ function tutarFormatla(input) {
         window.currentStats = data;
             window._gramKur = Number(data.gramAltinKuru) || 0;
         window._gramKur = data.gramAltinKuru ? Number(data.gramAltinKuru) : 0;
+            console.log("gramAltinKuru:", data.gramAltinKuru, "| _gramKur:", window._gramKur);
         window._usdKur = data.usdRate ? Number(data.usdRate) : 0;
         window._euroKur = data.euroRate ? Number(data.euroRate) : 0;
         window.tarihceData = data.tarihce || [];
